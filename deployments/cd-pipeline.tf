625c3cf9ff7a516330dd3b82df2ae4bb40edf38b
as027811 <alex.bezek@cerner.com>
2019-11-30 21:01:35 -0500

    Updates

--- deployments/cd-pipeline.tf
+++ deployments/cd-pipeline.tf
@@ -10,7 +10,7 @@ resource "aws_codebuild_project" "build" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image = "aws/codebuild/standard:3.0"
+    image = "ruby:2.6.5"
     type = "LINUX_CONTAINER"
 
     environment_variable {
